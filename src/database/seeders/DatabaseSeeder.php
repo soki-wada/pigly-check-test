c0218db980f8e14c27f8cfdbc12b37dda76a1146
soki_wada <swmk1sb@gmail.com>
2025-05-29 08:48:02 +0900

    complete

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call(UsersTableSeeder::class);
-        $this->call(WeightTargetsTableSeeder::class);
+        $this->call(WeightTargetTableSeeder::class);
         $this->call(WeightLogsTableSeeder::class);
     }
 }
