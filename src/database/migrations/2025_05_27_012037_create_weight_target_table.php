6f3378397c03e92972fb331cc8b63aa9ae362bce
soki_wada <swmk1sb@gmail.com>
2025-05-29 08:46:35 +0900

    complete

--- src/database/migrations/2025_05_27_012037_create_weight_target_table.php
+++ src/database/migrations/2025_05_27_012037_create_weight_target_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateWeightTargetsTable extends Migration
+class CreateWeightTargetTable extends Migration
 {
     /**
      * Run the migrations.
